154b7de8d33c9c3a90cbe06419671a62484fd20f
Matthew Lehner <matthew@mpl.io>
2017-12-01 20:27:48 -0800

    working site.

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,26 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
+    title: `Gatsby Default Starter`
   },
-  plugins: [`gatsby-plugin-react-helmet`],
-}
+  plugins: [
+    `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-catch-links`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/src/pages`,
+        name: "pages"
+      }
+    },
+    {
+      resolve: "gatsby-transformer-remark",
+      options: {
+        plugins: [
+          `gatsby-remark-prismjs`,
+          `gatsby-remark-copy-linked-files`,
+          `gatsby-remark-images`
+        ] // just in case those previously mentioned remark plugins sound cool :)
+      }
+    }
+  ]
+};
