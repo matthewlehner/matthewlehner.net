c3e88f0a4acfc5d0de75e26b5d9bf4149de56c74
Matthew Lehner <matthew@mpl.io>
2018-06-18 21:24:04 -0700

    Fixes rss feed generation.

--- gatsby-config.js
+++ gatsby-config.js
@@ -70,6 +70,7 @@ module.exports = {
                      frontmatter {
                        title
                        date
+                       path
                      }
                    }
                  }
